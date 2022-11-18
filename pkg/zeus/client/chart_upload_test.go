b474926d0914376a2d2c93e6b5504e4674f9c68a
alex <alexg89@vt.edu>
2022-11-17 16:12:08 -0800

    adds zeus client test base, additional tests

--- pkg/zeus/client/chart_upload_test.go
+++ pkg/zeus/client/chart_upload_test.go
@@ -1,8 +1,10 @@
 package zeus_client
 
 // TestChartUpload will return a topology id associated with this workload
-func (t *ZeusClientTestSuite) TestChartUpload() {
+func (t *ZeusClientTestSuite) TestChartUpload() int {
 	resp, err := t.ZeusTestClient.UploadChart(ctx, demoChartPath, uploadChart)
 	t.Require().Nil(err)
 	t.Assert().NotZero(resp.ID)
+
+	return resp.ID
 }
