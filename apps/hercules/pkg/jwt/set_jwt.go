19cbe8aa458e05288e360c671505656e97adaee0
alex <alexg89@vt.edu>
2022-11-18 18:59:52 -0800

    adds hercules docker & default jwt

--- apps/hercules/pkg/jwt/set_jwt.go
+++ apps/hercules/pkg/jwt/set_jwt.go
@@ -1,11 +1,26 @@
 package hercules_jwt
 
 import (
+	"github.com/rs/zerolog/log"
 	filepaths "github.com/zeus-fyi/zeus/pkg/utils/file_io/lib/v0/paths"
 )
 
 func SetToken(p filepaths.Path, token string) error {
-
 	err := p.WriteFileInPath([]byte(token))
 	return err
+}
+
+func CheckIfJwtTokenExistsElseWriteDefault(p filepaths.Path, tokenFileName, defaultToken string) error {
+	p.FnIn = tokenFileName
+	p.FnOut = tokenFileName
+
+	if !p.FileInPathExists() {
+		p.FnOut = "jwt.hex"
+		err := p.WriteFileInPath([]byte(defaultToken))
+		if err != nil {
+			log.Err(err).Msg("error setting jwt token")
+		}
+		return err
+	}
+	return nil
 }
--- pkg/crypto/jwt/jwt_test.go
+++ pkg/crypto/jwt/jwt_test.go
@@ -1,6 +1,7 @@
 package aegis_jwt
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -14,6 +15,7 @@ func (s *JWTTestSuite) TestJWT() {
 	j := NewAegisJWT()
 	j.GenerateJwtTokenString()
 	s.Assert().NotEmpty(j.Raw)
+	fmt.Println(j.Raw)
 }
 
 func TestJWTTestSuite(t *testing.T) {
