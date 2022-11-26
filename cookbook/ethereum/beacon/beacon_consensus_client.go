1650a08291e4d7ccb4be5958439fdb93f29d181c
alex <alexg89@vt.edu>
2022-11-25 18:27:19 -0800

    adds ingress & beacon setup in cookbook

---
+++ cookbook/ethereum/beacon/beacon_consensus_client.go
@@ -0,0 +1,35 @@
+package beacon_cookbook
+
+import (
+	"fmt"
+	"time"
+
+	filepaths "github.com/zeus-fyi/zeus/pkg/utils/file_io/lib/v0/paths"
+	strings_filter "github.com/zeus-fyi/zeus/pkg/utils/strings"
+	"github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_req_types"
+)
+
+// set your own topologyID here after uploading a chart workload
+var deployConsensusClientKnsReq = zeus_req_types.TopologyDeployRequest{
+	TopologyID: 0,
+	CloudCtxNs: beaconCloudCtxNs,
+}
+
+// chart workload metadata
+var consensusClientChart = zeus_req_types.TopologyCreateRequest{
+	TopologyName:     "lighthouse",
+	ChartName:        "lighthouse-hercules",
+	ChartDescription: "lighthouse-hercules",
+	Version:          fmt.Sprintf("v0.0.%d", time.Now().Unix()),
+}
+
+// DirOut is where it will write a copy of the chart you uploaded, which helps verify the workload is correct
+var beaconConsensusClientChartPath = filepaths.Path{
+	PackageName: "",
+	DirIn:       "./ethereum/beacon/infra",
+	DirOut:      "./ethereum/outputs",
+	FnIn:        "lighthouse", // filename for your gzip workload
+	FnOut:       "",
+	Env:         "",
+	FilterFiles: strings_filter.FilterOpts{},
+}
