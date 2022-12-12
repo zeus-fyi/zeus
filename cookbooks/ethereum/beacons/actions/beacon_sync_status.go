9a15f3600c2ac13465bd58738de42af4b503296c
alex <alexg89@vt.edu>
2022-12-11 23:47:17 -0800

    updates to use embdedded

--- cookbooks/ethereum/beacons/actions/beacon_sync_status.go
+++ cookbooks/ethereum/beacons/actions/beacon_sync_status.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 
 	"github.com/rs/zerolog/log"
-	beacon_cookbooks "github.com/zeus-fyi/zeus/cookbooks/ethereum/beacons"
 	client_consts "github.com/zeus-fyi/zeus/cookbooks/ethereum/beacons/constants"
 	strings_filter "github.com/zeus-fyi/zeus/pkg/utils/strings"
 	zeus_pods_reqs "github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_req_types/pods"
@@ -18,9 +17,8 @@ func (b *BeaconActionsClient) GetConsensusClientSyncStatus(ctx context.Context) 
 		Ports:      client_consts.GetClientBeaconPortsHTTP(b.ConsensusClient),
 	}
 	filter := strings_filter.FilterOpts{Contains: b.ConsensusClient}
-	routeHeader := beacon_cookbooks.DeployConsensusClientKnsReq
 	par := zeus_pods_reqs.PodActionRequest{
-		TopologyDeployRequest: routeHeader,
+		TopologyDeployRequest: b.BeaconKnsReq,
 		Action:                zeus_pods_reqs.PortForwardToAllMatchingPods,
 		ClientReq:             &cliReq,
 		FilterOpts:            &filter,
