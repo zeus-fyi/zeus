2a483a19baa0730f8f49cb770087a7491ab796df
alex <alexg89@vt.edu>
2022-12-15 11:13:08 -0800

    use snapshot app for jwt init

--- apps/hercules/server/hercules.go
+++ apps/hercules/server/hercules.go
@@ -7,21 +7,15 @@ import (
 	hercules_router "github.com/zeus-fyi/hercules/api"
 	v1_common_routes "github.com/zeus-fyi/hercules/api/v1/common"
 	aegis_inmemdbs "github.com/zeus-fyi/zeus/pkg/aegis/inmemdbs"
-	init_jwt "github.com/zeus-fyi/zeus/pkg/aegis/jwt"
 	filepaths "github.com/zeus-fyi/zeus/pkg/utils/file_io/lib/v0/paths"
 )
 
 var cfg = Config{}
 var env string
 var dataDir filepaths.Path
-var jwtToken string
-var useDefaultToken bool
 var bucketURL string
 
 func Hercules() {
-	if useDefaultToken {
-		_ = init_jwt.SetTokenToDefault(dataDir, "jwt.hex", jwtToken)
-	}
 	cfg.Host = "0.0.0.0"
 	srv := NewHerculesServer(cfg)
 	log.Info().Msg("Hercules: server starting")
@@ -41,8 +35,6 @@ func init() {
 	Cmd.Flags().StringVar(&dataDir.DirIn, "dataDirIn", "/data", "data directory location")
 	Cmd.Flags().StringVar(&env, "env", "local", "environment")
 	// uses a default token for demo, set your own jwt for production usage if desired
-	Cmd.Flags().StringVar(&jwtToken, "jwt", "0x6ad1acdc50a4141e518161ab2fe2bf6294de4b4d48bf3582f22cae8113f0cadc", "set jwt in datadir")
-	Cmd.Flags().BoolVar(&useDefaultToken, "useDefaultToken", true, "use default jwt token")
 
 	// you can contact us for a free bucket url to download blockchain data to bootstart your node
 	Cmd.Flags().StringVar(&bucketURL, "bucketUrl", "", "presigned bucket url for downloading chain snapshot data")
