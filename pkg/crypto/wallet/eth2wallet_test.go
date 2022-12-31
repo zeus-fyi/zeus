8c189e6bb6f2c20c4a8b28b40484cb6131576065
alex <alexg89@vt.edu>
2022-12-30 19:01:15 -0800

    encrypts to keystore EIP-2335 format

--- pkg/crypto/wallet/eth2wallet_test.go
+++ pkg/crypto/wallet/eth2wallet_test.go
@@ -2,6 +2,7 @@ package wallet
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 	"testing"
@@ -10,13 +11,25 @@ import (
 	"github.com/wealdtech/go-ed25519hd"
 	util "github.com/wealdtech/go-eth2-util"
 	e2wallet "github.com/wealdtech/go-eth2-wallet"
+	keystorev4 "github.com/wealdtech/go-eth2-wallet-encryptor-keystorev4"
 	scratch "github.com/wealdtech/go-eth2-wallet-store-scratch"
 	bls_signer "github.com/zeus-fyi/zeus/pkg/crypto/bls"
 	aegis_random "github.com/zeus-fyi/zeus/pkg/crypto/random"
+	filepaths "github.com/zeus-fyi/zeus/pkg/utils/file_io/lib/v0/paths"
+	strings_filter "github.com/zeus-fyi/zeus/pkg/utils/strings"
+	"github.com/zeus-fyi/zeus/test/configs"
 )
 
 type WalletTestSuite struct {
 	suite.Suite
+}
+
+var depositDataPath = filepaths.Path{
+	PackageName: "",
+	DirOut:      "../mocks/validator_keys",
+	FnOut:       "",
+	Env:         "",
+	FilterFiles: strings_filter.FilterOpts{},
 }
 
 func (s *WalletTestSuite) TestHDWalletCreation() {
@@ -43,6 +56,7 @@ func (s *WalletTestSuite) TestHDWalletCreation() {
 	for wallet := range e2wallet.Wallets() {
 		fmt.Printf("Found wallet %s\n", wallet.Name())
 		for account := range wallet.Accounts(ctx) {
+
 			fmt.Printf("Wallet %s has account %s\n", wallet.Name(), account.Name())
 		}
 	}
@@ -57,6 +71,24 @@ func (s *WalletTestSuite) TestHDWalletCreation() {
 	sk, err = util.PrivateKeyFromSeedAndPath(seed, path)
 	s.Require().Nil(err)
 	fmt.Println(bls_signer.ConvertBytesToString(sk.Marshal()))
+
+	ks := keystorev4.New()
+
+	enc, err := ks.Encrypt(sk.Marshal(), password)
+	s.Require().Nil(err)
+	fmt.Println(enc)
+
+	b, err := json.Marshal(enc)
+	s.Require().Nil(err)
+
+	configs.ForceDirToConfigLocation()
+
+	slashSplit := strings.Split(path, "/")
+	underScoreStr := strings.Join(slashSplit, "_")
+
+	depositDataPath.FnOut = fmt.Sprintf("keystore-ephemeral_%s", underScoreStr)
+	err = depositDataPath.WriteToFileOutPath(b)
+	s.Require().Nil(err)
 }
 
 func TestWalletTestSuite(t *testing.T) {
