db6768055ca86dd94a810e3cf02b9cd21f360980
alex <alexg89@vt.edu>
2023-01-02 12:38:14 -0800

    import keystore 2 web3signer working

--- cookbooks/ethereum/web3signers/actions/web3signer_importer_test.go
+++ cookbooks/ethereum/web3signers/actions/web3signer_importer_test.go
@@ -15,7 +15,7 @@ var keystorePath = filepaths.Path{
 	PackageName: "",
 	DirIn:       "./ethereum/automation/validator_keys/ephemery",
 	DirOut:      "./ethereum/automation/validator_keys/ephemery",
-	FnIn:        "keystore-ephemery-m_12381_3600_2_0_0.json",
+	FnIn:        "keystore-ephemery-m_12381_3600_0_0_0.json",
 	FnOut:       "",
 	Env:         "",
 	FilterFiles: strings_filter.FilterOpts{},
@@ -23,9 +23,17 @@ var keystorePath = filepaths.Path{
 
 var ctx = context.Background()
 
+func (t *EthereumWeb3SignerCookbookTestSuite) TestImportWeb3SignerKeysViaKeystoreAPI() {
+	kns := validator_cookbooks.ValidatorCloudCtxNs
+	w3 := Web3SignerActionsClient{t.ZeusTestClient}
+	resp, err := w3.ImportKeystores(ctx, kns, keystorePath, t.Tc.HDWalletPassword)
+	t.Require().Nil(err)
+	t.Assert().NotEmpty(resp)
+}
+
 func (t *EthereumWeb3SignerCookbookTestSuite) TestReadKeystoreFile() {
 	k := Web3SignerKeystores{}
-	k.ReadKeystore(ctx, keystorePath)
+	k.ReadKeystoreDirAndAppendPw(ctx, keystorePath, t.Tc.HDWalletPassword)
 	t.Assert().NotEmpty(k.Keystores)
 }
 
