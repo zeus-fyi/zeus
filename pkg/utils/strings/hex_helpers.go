9a432e0eb041ffafe39fc44dbf183e288935dd3a
alex <alexg89@vt.edu>
2023-01-19 12:58:31 -0800

    refactor

--- pkg/utils/strings/hex_helpers.go
+++ pkg/utils/strings/hex_helpers.go
@@ -1,23 +1,8 @@
 package strings_filter
 
-import "net/url"
-
 func AddHexPrefix(hex string) string {
 	if len(hex) >= 2 && hex[0:2] == "0x" {
 		return hex
 	}
 	return "0x" + hex
-}
-
-func ValidateHttpsURL(urlString string) bool {
-	// Parse the URL
-	urlParsed, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return false
-	}
-	// Check that it's an HTTPS URL
-	if urlParsed.Scheme != "https" {
-		return false
-	}
-	return true
 }
