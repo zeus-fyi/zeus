d512de3aa83425e4d66e357ba31853919657eb15
alex <alexg89@vt.edu>
2023-01-25 17:45:33 -0800

    adds group verify on new signatures

---
+++ pkg/aegis/inmemdbs/validator_signatures_verified.go
@@ -0,0 +1,64 @@
+package aegis_inmemdbs
+
+import (
+	"context"
+	"encoding/hex"
+	"errors"
+
+	"github.com/rs/zerolog/log"
+	types "github.com/wealdtech/go-eth2-types/v2"
+	strings_filter "github.com/zeus-fyi/zeus/pkg/utils/strings"
+)
+
+// VerifySignatures returns a slice of pubkeys that have been verified with the given signed message, it returns the pubkeys with a 0x prefix string
+func (sr *EthereumBLSKeySignatureResponses) VerifySignatures(ctx context.Context, sigRequests EthereumBLSKeySignatureRequests) ([]string, error) {
+	if len(sr.Map) <= 0 {
+		return []string{}, nil
+	}
+
+	verifiedKeys := make([]string, len(sr.Map))
+
+	i := 0
+	for k, sigResp := range sr.Map {
+
+		msgStr, ok := sigRequests.Map[k]
+
+		if !ok {
+			err := errors.New("pubkey not in signature request message")
+			log.Ctx(ctx).Err(err)
+			return []string{}, err
+		}
+
+		sigHexStr, err := hex.DecodeString(sigResp.Signature)
+
+		if err != nil {
+			log.Ctx(ctx).Err(err)
+			return nil, err
+		}
+
+		sig, err := types.BLSSignatureFromBytes(sigHexStr)
+		if err != nil {
+			log.Ctx(ctx).Err(err)
+			return nil, err
+		}
+
+		pubkeyHexStr, err := hex.DecodeString(k)
+		if err != nil {
+			log.Ctx(ctx).Err(err)
+			return nil, err
+		}
+		pubkey, err := types.BLSPublicKeyFromBytes(pubkeyHexStr)
+		if err != nil {
+			log.Ctx(ctx).Err(err)
+			return nil, err
+		}
+		if !sig.Verify([]byte(msgStr.Message), pubkey) {
+			err = errors.New("signature does not map to expected pubkey")
+			log.Ctx(ctx).Err(err)
+			return []string{}, err
+		}
+		verifiedKeys[i] = strings_filter.AddHexPrefix(k)
+		i++
+	}
+	return verifiedKeys, nil
+}
