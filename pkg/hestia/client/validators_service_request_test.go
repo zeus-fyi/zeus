957a42767ef9d49bb3c7db37de7683692c77f123
alex <alexg89@vt.edu>
2023-01-27 17:03:48 -0800

    updates req types

--- pkg/hestia/client/validators_service_request_test.go
+++ pkg/hestia/client/validators_service_request_test.go
@@ -17,10 +17,19 @@ func (t *HestiaClientTestSuite) TestValidatorServiceRequest() {
 	sr := hestia_req_types.ServiceRequestWrapper{
 		GroupName:         "test",
 		ProtocolNetworkID: hestia_req_types.EthereumEphemeryProtocolNetworkID,
-		FeeRecipient:      t.TestAccount1.PublicKey(),
 		Enabled:           true,
 	}
-	hs.CreateValidatorServiceRequest(dp, sr)
+	keyOne := hestia_req_types.ValidatorServiceOrgGroup{
+		Pubkey:       "0x8a7addbf2857a72736205d861169c643545283a74a1ccb71c95dd2c9652acb89de226ca26d60248c4ef9591d7e010288",
+		FeeRecipient: "0xF7Ab1d834Cd0A33691e9A750bD720cb6436cA1B9",
+	}
+	keyTwo := hestia_req_types.ValidatorServiceOrgGroup{
+		Pubkey:       "0x9a7addbf2857a72736205d861169c643545283a74a1ccb71c95dd2c9652acb89de226ca26d60248c4ef9591d7e010288",
+		FeeRecipient: "0xF7Ab1d834Cd0A33691e9A750bD720cb6436cA1B9",
+	}
+	pubkeys := hestia_req_types.ValidatorServiceOrgGroupSlice{keyOne, keyTwo}
+
+	hs.CreateValidatorServiceRequest(pubkeys, sr)
 	t.Assert().NotEmpty(hs)
 	resp, err := t.HestiaTestClient.ValidatorsServiceRequest(ctx, hs)
 	t.Require().Nil(err)
