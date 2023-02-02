34a04001f003ffe1d5552383971f71ad8b40684f
alex <alexg89@vt.edu>
2023-02-02 10:59:38 -0800

    add todo

--- pkg/zeus/workload_config_drivers/deployments.go
+++ pkg/zeus/workload_config_drivers/deployments.go
@@ -17,6 +17,7 @@ func (d *DeploymentDriver) SetDeploymentConfigs(sts *v1.Deployment) {
 		return
 	}
 
+	// TODO refactor into pod template spec config, then share w/sts + here
 	// init containers
 	for i, c := range sts.Spec.Template.Spec.InitContainers {
 		if v, ok := d.ContainerDrivers[c.Name]; ok {
@@ -24,6 +25,7 @@ func (d *DeploymentDriver) SetDeploymentConfigs(sts *v1.Deployment) {
 			sts.Spec.Template.Spec.InitContainers[i] = c
 		}
 	}
+	// containers
 	for i, c := range sts.Spec.Template.Spec.Containers {
 		if v, ok := d.ContainerDrivers[c.Name]; ok {
 			v.SetContainerConfigs(&c)
