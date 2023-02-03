1b0129e9a8a04a669891cdc0c2c70c9fac634017
alex <alexg89@vt.edu>
2023-02-02 18:14:26 -0800

    bls serverless signer

--- apps/cookbooks/serverless/ethereum/signatures/main.go
+++ apps/cookbooks/serverless/ethereum/signatures/main.go
@@ -2,14 +2,43 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/go-resty/resty/v2"
+	"github.com/rs/zerolog/log"
+	bls_signer "github.com/zeus-fyi/zeus/pkg/crypto/bls"
 )
 
-func HandleRequest(ctx context.Context, secretID string, msg any) (string, error) {
-	return "Hello from Lambda!", nil
+const (
+	SessionToken    = "AWS_SESSION_TOKEN"
+	SecretsHeader   = "X-Aws-Parameters-Secrets-Token"
+	SecretsPortHTTP = 2773
+)
+
+func HandleSignRequestBLS(ctx context.Context, secretName, keyName, msg string) (string, error) {
+	headerValue := os.Getenv(SessionToken)
+	r := resty.New()
+	respJSON := make(map[string]any)
+	_, err := r.R().
+		SetHeader(SecretsHeader, headerValue).
+		SetResult(&respJSON).
+		Get(fmt.Sprintf("http://localhost:%d/secretsmanager/get?secretId=%s", SecretsPortHTTP, secretName))
+	if err != nil {
+		log.Ctx(ctx).Err(err)
+		return "", err
+	}
+	sk, ok := respJSON[keyName]
+	if !ok {
+		log.Ctx(ctx).Warn().Interface("key", keyName).Msg("no value found for secret key")
+		return "", err
+	}
+	acc := bls_signer.NewEthSignerBLSFromExistingKey(sk.(string))
+	sig := acc.Sign([]byte(msg))
+	return bls_signer.ConvertBytesToString(sig.Marshal()), nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(HandleSignRequestBLS)
 }
