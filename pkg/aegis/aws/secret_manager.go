277039c72192f9e8fb9e5a2e13d18e0f45e09439
alex <alexg89@vt.edu>
2023-02-03 16:34:09 -0800

    cleanup

--- pkg/aegis/aws/secret_manager.go
+++ pkg/aegis/aws/secret_manager.go
@@ -40,28 +40,11 @@ func InitSecretsManager(ctx context.Context, auth AuthAWS) (SecretsManagerAuthAW
 }
 
 func (s *SecretsManagerAuthAWS) GetSecret(ctx context.Context, si SecretInfo) (string, error) {
-	input := &secretsmanager.GetSecretValueInput{
-		SecretId:     aws.String(si.Name),
-		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
-	}
-
-	result, err := s.GetSecretValue(ctx, input)
-	if err != nil {
-		// For a list of exceptions thrown, see
-		// https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-		log.Ctx(ctx).Err(err)
-		return "", err
-	}
-
-	// Decrypts secret using the associated KMS key.
-	var secretString = *result.SecretString
-	m := make(map[string]any)
-	err = json.Unmarshal([]byte(secretString), &m)
+	m, err := s.GetSecretsJSON(ctx, si)
 	if err != nil {
 		log.Ctx(ctx).Err(err)
 		return "", err
 	}
-
 	secretValue, ok := m[si.Key]
 	if !ok {
 		log.Ctx(ctx).Warn().Interface("key", si.Key).Msg("no value found for secret key")
