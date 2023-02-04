bc20c7cfe56ad2a1e636d0a65d1d2a97363bce69
alex <alexg89@vt.edu>
2023-02-03 19:32:45 -0800

    cleanup

--- apps/cookbooks/serverless/ethereum/bls_signatures/go.mod
+++ apps/cookbooks/serverless/ethereum/bls_signatures/go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/valyala/fastjson v1.6.4 // indirect
 	golang.org/x/net v0.3.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- apps/cookbooks/serverless/ethereum/bls_signatures/main.go
+++ apps/cookbooks/serverless/ethereum/bls_signatures/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
 	"github.com/go-resty/resty/v2"
 	"github.com/rs/zerolog/log"
-
-	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
-	aegis_inmemdbs "github.com/zeus-fyi/zeus/pkg/aegis/inmemdbs"
+	"github.com/valyala/fastjson"
 	bls_signer "github.com/zeus-fyi/zeus/pkg/crypto/bls"
+
+	aegis_inmemdbs "github.com/zeus-fyi/zeus/pkg/aegis/inmemdbs"
 )
 
 const (
@@ -25,40 +29,79 @@ var (
 )
 
 type SignRequestsEvent struct {
-	SecretName string `json:"secretName"`
-	aegis_inmemdbs.EthereumBLSKeySignatureRequests
+	SecretName        string                                         `json:"secretName"`
+	SignatureRequests aegis_inmemdbs.EthereumBLSKeySignatureRequests `json:"signatureRequests"`
 }
 
-func HandleEthSignRequestBLS(ctx context.Context, event SignRequestsEvent) (aegis_inmemdbs.EthereumBLSKeySignatureResponses, error) {
-	sigResponsesMap := make(map[string]aegis_inmemdbs.EthereumBLSKeySignatureResponse)
-	batchSigResponses := aegis_inmemdbs.EthereumBLSKeySignatureResponses{
-		Map: sigResponsesMap,
+func HandleEthSignRequestBLS(ctx context.Context, event events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
+	ApiResponse := events.APIGatewayProxyResponse{}
+	// Switch for identifying the HTTP request
+	switch event.HTTPMethod {
+	case "GET":
+	case "POST":
+		//validates json and returns error if not working
+		err := fastjson.Validate(event.Body)
+		if err != nil {
+			body := "Error: Invalid JSON payload ||| " + fmt.Sprint(err) + " Body Obtained" + "||||" + event.Body
+			ApiResponse = events.APIGatewayProxyResponse{Body: body, StatusCode: 500}
+		} else {
+			resp, serr := SignMessages(ctx, event.Body)
+			if serr != nil {
+				body := "Error: Invalid JSON payload ||| " + fmt.Sprint(serr) + " Body Obtained" + "||||" + event.Body
+				ApiResponse = events.APIGatewayProxyResponse{Body: body, StatusCode: 500}
+				return ApiResponse
+			}
+			ApiResponse = events.APIGatewayProxyResponse{Body: resp, StatusCode: 200}
+		}
+	}
+	return ApiResponse
+
+}
+func SignMessages(ctx context.Context, body string) (string, error) {
+	b, err := json.Marshal(body)
+	if err != nil {
+		log.Ctx(ctx).Err(err)
+		return "", err
+	}
+
+	sr := SignRequestsEvent{}
+	err = json.Unmarshal(b, &sr)
+	if err != nil {
+		log.Ctx(ctx).Err(err)
+		return "", err
+	}
+
+	if len(sr.SecretName) <= 0 {
+		return "", errors.New("no secret name provided")
 	}
 	headerValue := os.Getenv(SessionToken)
 	r := resty.New()
-	respJSON := make(map[string]any)
-	url := fmt.Sprintf("http://localhost:%d/secretsmanager/get?secretId=%s", SecretsPortHTTP, event.SecretName)
-	_, err := r.R().
+
+	var respJSON map[string]any
+	url := fmt.Sprintf("http://localhost:%d/secretsmanager/get?secretId=%s", SecretsPortHTTP, sr.SecretName)
+	log.Ctx(ctx).Info().Interface("url", url)
+
+	_, err = r.R().
 		SetHeader(SecretsHeader, headerValue).
 		SetResult(&respJSON).
 		Get(url)
 	if err != nil {
 		log.Ctx(ctx).Err(err)
-		return batchSigResponses, err
+		return "", err
 	}
-
-	for pubkey, msg := range event.Map {
+	signedResponses := aegis_inmemdbs.EthereumBLSKeySignatureResponses{Map: make(map[string]aegis_inmemdbs.EthereumBLSKeySignatureResponse)}
+	for pubkey, msg := range sr.SignatureRequests.Map {
 		sk, ok := respJSON[pubkey]
 		if !ok {
 			log.Ctx(ctx).Warn().Interface("key", pubkey).Msg("no value found for secret key")
 		} else {
 			acc := bls_signer.NewEthSignerBLSFromExistingKey(sk.(string))
 			sig := acc.Sign([]byte(msg.Message)).Marshal()
-			batchSigResponses.Map[pubkey] = aegis_inmemdbs.EthereumBLSKeySignatureResponse{Signature: bls_signer.ConvertBytesToString(sig)}
+			signedResponses.Map[pubkey] = aegis_inmemdbs.EthereumBLSKeySignatureResponse{Signature: bls_signer.ConvertBytesToString(sig)}
 		}
 	}
-
-	return batchSigResponses, nil
+	b, serr := json.Marshal(signedResponses)
+	return string(b), serr
 }
 
 func main() {
