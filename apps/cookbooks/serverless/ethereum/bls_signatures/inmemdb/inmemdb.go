ea1897458aebc2a3c1b310f7965d6f0436c72652
alex <alexg89@vt.edu>
2023-02-05 00:27:17 -0800

    implement age lightweight bulk validator encrypt

--- apps/cookbooks/serverless/ethereum/bls_signatures/inmemdb/inmemdb.go
+++ apps/cookbooks/serverless/ethereum/bls_signatures/inmemdb/inmemdb.go
@@ -27,7 +27,7 @@ func ImportIntoInMemDB(ctx context.Context, password string) {
 		aegis_inmemdbs.InitValidatorDB()
 	}
 	vs := aegis_inmemdbs.DecryptedValidators{HDPassword: password}
-	err := KeystorePath.WalkAndApplyFuncToFileType(".json", vs.ReadValidatorFromKeystore)
+	err := KeystorePath.WalkAndApplyFuncToFileType(".json", vs.ReadValidatorsFromKeystores)
 	if err != nil {
 		log.Ctx(ctx).Err(err)
 		panic(err)
--- pkg/crypto/age/age.go
+++ pkg/crypto/age/age.go
@@ -1,5 +1,12 @@
 package age_encryption
 
+import (
+	"errors"
+	"github.com/rs/zerolog/log"
+	"github.com/zeus-fyi/zeus/pkg/utils/file_io/lib/v0/compression"
+	filepaths "github.com/zeus-fyi/zeus/pkg/utils/file_io/lib/v0/paths"
+)
+
 type Age struct {
 	agePrivateKey string
 	agePublicKey  string
@@ -11,4 +18,31 @@ func NewAge(privKey, pubKey string) Age {
 		agePublicKey:  pubKey,
 	}
 	return a
+}
+
+/* GzipAndEncrypt, use this format
+p := filepaths.Path{
+		DirIn:       "./secrets",
+		FnIn:        "secrets",
+	}
+*/
+
+func (a *Age) GzipAndEncrypt(p *filepaths.Path) error {
+	if p == nil {
+		return errors.New("need to include a path")
+	}
+	c := compression.NewCompression()
+
+	err := c.GzipCompressDir(p)
+	if err != nil {
+		log.Err(err)
+		return err
+	}
+
+	err = a.Encrypt(p)
+	if err != nil {
+		log.Err(err)
+		return err
+	}
+	return err
 }
