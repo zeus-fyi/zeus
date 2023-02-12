35d033217bfc71667b03c9be4d6815a70e2ac649
alex <alexg89@vt.edu>
2023-02-12 01:18:57 -0800

    adds pause & start to os

---
+++ cookbooks/ethereum/beacons/actions/pause_client.go
@@ -0,0 +1,73 @@
+package beacon_actions
+
+import (
+	"context"
+	"errors"
+	"github.com/rs/zerolog/log"
+	client_consts "github.com/zeus-fyi/zeus/cookbooks/ethereum/beacons/constants"
+	"github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_common_types"
+	"github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_req_types"
+	zeus_config_map_reqs "github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_req_types/config_maps"
+	zeus_pods_reqs "github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_req_types/pods"
+)
+
+// set your own topologyID here after uploading a chart workload
+var beaconKnsReq = zeus_req_types.TopologyDeployRequest{
+	TopologyID: 0,
+	CloudCtxNs: beaconCloudCtxNs,
+}
+
+var beaconCloudCtxNs = zeus_common_types.CloudCtxNs{
+	CloudProvider: "do",
+	Region:        "sfo3",
+	Context:       "do-sfo3-dev-do-sfo3-zeus",
+	Namespace:     "ethereum", // set with your own namespace
+	Env:           "dev",
+}
+
+var basePar = zeus_pods_reqs.PodActionRequest{
+	TopologyDeployRequest: beaconKnsReq,
+	PodName:               "",
+	FilterOpts:            nil,
+	ClientReq:             nil,
+	DeleteOpts:            nil,
+}
+
+func (b *BeaconActionsClient) PauseClient(ctx context.Context, cmName, clientName string) ([]byte, error) {
+	cmr := zeus_config_map_reqs.ConfigMapActionRequest{
+		TopologyDeployRequest: b.BeaconKnsReq,
+		Action:                zeus_config_map_reqs.SetOrCreateKeyFromExisting,
+		ConfigMapName:         cmName,
+		Keys: zeus_config_map_reqs.KeySwap{
+			KeyOne: "pause.sh",
+			KeyTwo: "start.sh",
+		},
+		FilterOpts: nil,
+	}
+	respCm, err := b.SetOrCreateKeyFromConfigMapKey(ctx, cmr)
+	if err != nil {
+		log.Ctx(ctx).Err(err).Interface("configMap", respCm).Msg("PauseConsensusClient: SetOrCreateKeyFromConfigMapKey")
+		return nil, err
+	}
+	if client_consts.IsConsensusClient(clientName) {
+		b.ConsensusClient = clientName
+		basePar.TopologyDeployRequest = b.BeaconKnsReq
+		resp, cerr := b.RestartConsensusClientPods(ctx, basePar)
+		if cerr != nil {
+			log.Ctx(ctx).Err(cerr).Msg("PauseConsensusClient: RestartConsensusClientPods")
+			return nil, cerr
+		}
+		return resp, cerr
+	}
+	if client_consts.IsExecClient(clientName) {
+		b.ExecClient = clientName
+		basePar.TopologyDeployRequest = b.BeaconKnsReq
+		resp, cerr := b.RestartExecClientPods(ctx, basePar)
+		if cerr != nil {
+			log.Ctx(ctx).Err(cerr).Msg("PauseConsensusClient: RestartExecClientPods")
+			return nil, cerr
+		}
+		return resp, cerr
+	}
+	return nil, errors.New("invalid consensus exec client supplied")
+}
---
+++ cookbooks/ethereum/beacons/actions/start_client.go
@@ -0,0 +1,74 @@
+package beacon_actions
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/rs/zerolog/log"
+	client_consts "github.com/zeus-fyi/zeus/cookbooks/ethereum/beacons/constants"
+	strings_filter "github.com/zeus-fyi/zeus/pkg/utils/strings"
+	zeus_config_map_reqs "github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_req_types/config_maps"
+	zeus_pods_reqs "github.com/zeus-fyi/zeus/pkg/zeus/client/zeus_req_types/pods"
+)
+
+func (b *BeaconActionsClient) StartConsensusClient(ctx context.Context) ([]byte, error) {
+	return b.StartClient(ctx, "cm-lighthouse", client_consts.Lighthouse)
+}
+
+func (b *BeaconActionsClient) StartExecClient(ctx context.Context) ([]byte, error) {
+	return b.StartClient(ctx, "cm-geth", client_consts.Geth)
+}
+
+func (b *BeaconActionsClient) StartClient(ctx context.Context, cmName, clientName string) ([]byte, error) {
+	fo := strings_filter.FilterOpts{
+		DoesNotStartWithThese: nil,
+		StartsWithThese:       nil,
+		StartsWith:            "",
+		Contains:              clientName,
+		DoesNotInclude:        nil,
+	}
+	par := zeus_pods_reqs.PodActionRequest{
+		TopologyDeployRequest: b.BeaconKnsReq,
+		Action:                zeus_pods_reqs.DeleteAllPods,
+		PodName:               "",
+		ContainerName:         "",
+		FilterOpts:            &fo,
+		ClientReq:             nil,
+		LogOpts:               nil,
+		DeleteOpts:            nil,
+	}
+	cmReq := zeus_config_map_reqs.ConfigMapActionRequest{
+		TopologyDeployRequest: b.BeaconKnsReq,
+		Action:                zeus_config_map_reqs.SetOrCreateKeyFromExisting,
+		ConfigMapName:         cmName,
+		Keys: zeus_config_map_reqs.KeySwap{
+			KeyOne: fmt.Sprintf("%s.sh", clientName),
+			KeyTwo: "start.sh",
+		},
+		FilterOpts: nil,
+	}
+	respCm, err := b.SetOrCreateKeyFromConfigMapKey(ctx, cmReq)
+	if err != nil {
+		log.Ctx(ctx).Err(err).Interface("configMap", respCm).Msg("PauseConsensusClient: SetOrCreateKeyFromConfigMapKey")
+		return nil, err
+	}
+	if client_consts.IsConsensusClient(clientName) {
+		b.ConsensusClient = clientName
+		resp, cerr := b.RestartConsensusClientPods(ctx, par)
+		if cerr != nil {
+			log.Ctx(ctx).Err(cerr).Msg("PauseConsensusClient: RestartConsensusClientPods")
+			return nil, cerr
+		}
+		return resp, cerr
+	}
+	if client_consts.IsExecClient(clientName) {
+		b.ExecClient = clientName
+		resp, cerr := b.RestartExecClientPods(ctx, par)
+		if cerr != nil {
+			log.Ctx(ctx).Err(cerr).Msg("PauseConsensusClient: RestartExecClientPods")
+			return nil, cerr
+		}
+		return resp, cerr
+	}
+	return nil, errors.New("invalid consensus exec client supplied")
+}
