cc9a02907ffe01ca1b0ff910ba3a3bddd66ddf54
alex <alexg89@vt.edu>
2023-03-15 17:36:49 -0700

    remove log

--- pkg/aegis/aws/iam/create_access_keys.go
+++ pkg/aegis/aws/iam/create_access_keys.go
@@ -2,6 +2,7 @@ package aegis_aws_iam
 
 import (
 	"context"
+
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/rs/zerolog/log"
 	aws_aegis_auth "github.com/zeus-fyi/zeus/pkg/aegis/aws/auth"
@@ -23,8 +24,5 @@ func (i *IAMClientAWS) CreateUserAccessKeys(ctx context.Context, userName string
 	}
 	auth.AccessKey = *createAccessKeyOutput.AccessKey.AccessKeyId
 	auth.SecretKey = *createAccessKeyOutput.AccessKey.SecretAccessKey
-	println("Access Key:", auth.AccessKey)
-	println("Secret Key:", auth.SecretKey)
-
 	return auth, nil
 }
