fe7f68ae27629bd56a76798e0083736969cba3e6
alex <alexg89@vt.edu>
2023-03-24 17:03:50 -0700

    adds tls, rules to ingress cfd

--- pkg/zeus/workload_config_drivers/ingresses.go
+++ pkg/zeus/workload_config_drivers/ingresses.go
@@ -23,10 +23,15 @@ func (i *IngressDriver) SetIngressConfigs(ing *v1.Ingress) {
 		tmp[nginxAuthURLAnnotation] = i.NginxAuthURL
 	}
 	if i.Ingress.Spec.TLS != nil {
-		ing.Spec.TLS = i.Ingress.Spec.TLS
+		for ind, _ := range i.Ingress.Spec.TLS {
+			ing.Spec.TLS[ind].SecretName = i.Spec.TLS[ind].SecretName
+			ing.Spec.TLS[ind].Hosts = i.Spec.TLS[ind].Hosts
+		}
 	}
 	if i.Ingress.Spec.Rules != nil {
-		ing.Spec.Rules = i.Ingress.Spec.Rules
+		for ind, _ := range i.Ingress.Spec.Rules {
+			ing.Spec.Rules[ind].Host = i.Ingress.Spec.Rules[ind].Host
+		}
 	}
 	ing.Annotations = tmp
 }
