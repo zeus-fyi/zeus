cd7ae89868384c97525fd40eda51a6b8bd5b05e2
alex <alexg89@vt.edu>
2023-05-26 23:31:52 -0700

    update imports

--- web3_actions/prep_signed_txs.go
+++ web3_actions/prep_signed_txs.go
@@ -28,7 +28,7 @@ func (w *Web3Actions) GetSignedSendTx(ctx context.Context, params SendEtherPaylo
 	}
 	log.Ctx(ctx).Info().Interface("chainID", chainID).Msg("GetSignedSendTx")
 	tx := types.NewTransaction(nonce, params.ToAddress, params.Amount, params.GasLimit, params.GasPrice, nil)
-	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), w.EcdsaPrivateKey())
+	signedTx, err := types.SignTx(tx, types.NewLondonSigner(chainID), w.EcdsaPrivateKey())
 	if err != nil {
 		log.Ctx(ctx).Err(err).Msg("Send: SignTx")
 		return nil, fmt.Errorf("cannot sign transaction: %v", err)
--- web3_actions/prep_smart_contract_signed_txs.go
+++ web3_actions/prep_smart_contract_signed_txs.go
@@ -38,7 +38,7 @@ func (w *Web3Actions) GetSignedTxToCallFunctionWithData(ctx context.Context, pay
 		return nil, fmt.Errorf("cannot get nonce: %v", err)
 	}
 	tx := types.NewTransaction(nonce, common.HexToAddress(payload.SmartContractAddr), payload.Amount, payload.GasLimit, payload.GasPrice, data)
-	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), w.EcdsaPrivateKey())
+	signedTx, err := types.SignTx(tx, types.NewLondonSigner(chainID), w.EcdsaPrivateKey())
 	if err != nil {
 		err = fmt.Errorf("cannot sign transaction: %v", err)
 		log.Ctx(ctx).Err(err).Msg("CallFunctionWithData: SignTx")
