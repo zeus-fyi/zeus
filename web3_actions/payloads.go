9aeba15bf80e7e332e8c06ff1d5383127bb8cc07
alex <alexg89@vt.edu>
2023-05-27 00:05:57 -0700

    adds dynamic fee fields

--- web3_actions/payloads.go
+++ web3_actions/payloads.go
@@ -21,20 +21,36 @@ type SendEtherPayload struct {
 	GasPriceLimits
 }
 
+func (tx *SendEtherPayload) EffectiveGasPrice(dst *big.Int, baseFee *big.Int) *big.Int {
+	if baseFee == nil {
+		return dst.Set(tx.GasFeeCap)
+	}
+	tip := dst.Sub(tx.GasFeeCap, baseFee)
+	if tip.Cmp(tx.GasTipCap) > 0 {
+		tip.Set(tx.GasTipCap)
+	}
+	return tip.Add(tip, baseFee)
+}
+
 type TransferArgs struct {
 	Amount    *big.Int
 	ToAddress common.Address
 }
 
 type GasPriceLimits struct {
-	GasPrice *big.Int
-	GasLimit uint64
+	GasPrice  *big.Int
+	GasLimit  uint64
+	GasTipCap *big.Int // a.k.a. maxPriorityFeePerGas
+	GasFeeCap *big.Int // a.k.a. maxFeePerGas
 }
+
 type CallMsg struct {
-	From     *common.Address // the sender of the 'transaction'
-	To       *common.Address // the destination contract (nil for contract creation)
-	Gas      uint64          // if 0, the call executes with near-infinite gas
-	GasPrice *big.Int        // wei <-> gas exchange ratio
-	Value    *big.Int        // amount of wei sent along with the call
-	Data     []byte          // input data, usually an ABI-encoded contract method invocation
+	From      *common.Address // the sender of the 'transaction'
+	To        *common.Address // the destination contract (nil for contract creation)
+	Gas       uint64          // if 0, the call executes with near-infinite gas
+	GasPrice  *big.Int        // wei <-> gas exchange ratio
+	GasTipCap *big.Int        // a.k.a. maxPriorityFeePerGas
+	GasFeeCap *big.Int        // a.k.a. maxFeePerGas
+	Value     *big.Int        // amount of wei sent along with the call
+	Data      []byte          // input data, usually an ABI-encoded contract method invocation
 }
