ed3cb19414aefda4c68f9bc5a2f9192709eca8ee
alex <alexg89@vt.edu>
2023-07-02 19:14:28 -0700

    adds test

--- pkg/zeus/iris_proxy_rules_configs/locked_sessions.go
+++ pkg/zeus/iris_proxy_rules_configs/locked_sessions.go
@@ -1,7 +1,8 @@
 package iris_proxy_rules_configs
 
 const (
-	SessionLockID = "Session-Lock-ID"
+	SessionLockID  = "Session-Lock-ID"
+	SessionLockTTL = "Session-Lock-TTL"
 )
 
 type IrisProxyRulesConfigs struct {
@@ -11,4 +12,8 @@ type IrisProxyRulesConfigs struct {
 
 func (i *IrisProxyRulesConfigs) AddSessionLockIDHeaderToHeaders(sessionID string) {
 	i.Headers[SessionLockID] = sessionID
+}
+
+func (i *IrisProxyRulesConfigs) AddSessionLockTTLHeaderToHeaders(ttl string) {
+	i.Headers[SessionLockTTL] = ttl
 }
