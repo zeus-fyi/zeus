9b09ca266e0a536af87df01accca8b6f81787a6b
alex <alexg89@vt.edu>
2023-08-20 19:48:00 -0700

    adds lb header

--- zeus/iris_programmable_proxy/v1beta/load_balancing.go
+++ zeus/iris_programmable_proxy/v1beta/load_balancing.go
@@ -1,9 +1,33 @@
 package iris_programmable_proxy_v1_beta
 
 const (
-	LoadBalancingStrategy = "Load-Balancing-Strategy"
+	LoadBalancingStrategy    = "X-Load-Balancing-Strategy"
+	Adaptive                 = "Adaptive"
+	AdaptiveLoadBalancingKey = "X-Adaptive-Metrics-Key"
+	EthereumJsonRPC          = "Ethereum"
 )
 
 func (i *IrisProxyRulesConfigs) AddLoadBalancingStrategyHeaderToHeaders(lbs string) {
 	i.Headers[LoadBalancingStrategy] = lbs
 }
+
+func (i *IrisProxyRulesConfigs) AddEthereumJsonRpcAdaptiveLoadBalancingStrategyHeaderToHeaders() {
+	i.Headers[LoadBalancingStrategy] = Adaptive
+	i.Headers[AdaptiveLoadBalancingKey] = EthereumJsonRPC
+}
+
+/*
+
+EthereumJsonRPC will build stats on the payload using the method value in the JSON RPC request,
+and is a reserved key-value pair for the Adaptive load balancing strategy designed for QuickNode's Marketplace.
+
+It uses generated table stats to optimize latency and throughput for Ethereum JSON RPC requests once enough samples
+have been taken.
+
+{
+	"jsonrpc": "2.0",
+	"method": "eth_blockNumber", // builds stats on this method value
+	"params": [],
+	"id": 1
+}
+*/
