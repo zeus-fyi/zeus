e209ccd6def606d8d12bf9085d348e6b8e5a7b6b
alex <alexg89@vt.edu>
2023-08-21 20:00:53 -0700

    adds iris etl operators wip

--- zeus/iris_programmable_proxy/v1beta/routing_procedures.go
+++ zeus/iris_programmable_proxy/v1beta/routing_procedures.go
@@ -1,5 +1,7 @@
 package iris_programmable_proxy_v1_beta
 
+import "time"
+
 const (
 	RoutingProcedureHeader = "X-Routing-Procedure"
 )
@@ -16,20 +18,34 @@ type IrisRoutingProcedure struct {
 	OrderedSteps []IrisRoutingProcedureStep `json:"steps"`
 }
 
-type IrisRoutingProcedureStep struct {
-	RoutingPath string `json:"routingPath"`
-	RestType    string `json:"restType"`
-	Payload     any    `json:"payload"`
+type BroadcastInstructions struct {
+	RoutingPath  string        `json:"routingPath"`
+	RestType     string        `json:"restType"`
+	MaxDuration  time.Duration `json:"maxRuntime"`
+	MaxTries     int           `json:"maxTries"`
+	RoutingTable string        `json:"routingTable"`
+}
 
-	TransformMap  map[string]IrisRoutingResponseETL `json:"transformMap"`
-	AggregateMap  map[string]IrisRoutingResponseETL `json:"aggregateMap"`
-	NextProcedure IrisRoutingProcedure              `json:"nextProcedure"`
+type IrisRoutingProcedureStep struct {
+	BroadcastInstructions BroadcastInstructions             `json:"broadcastInstructions,omitempty"`
+	TransformMap          map[string]IrisRoutingResponseETL `json:"transformMap,omitempty"`
+	AggregateMap          map[string]IrisRoutingResponseETL `json:"aggregateMap,omitempty"`
+	NextProcedure         *IrisRoutingProcedure             `json:"nextProcedure,omitempty"`
 }
 
 type IrisRoutingResponseETL struct {
 	//Operations []Operation `json:"operation"`
 	DataType string `json:"dataType"`
 	Result   any    `json:"result"`
+}
+
+func (r *IrisRoutingProcedure) GetNextProcedure() *IrisRoutingProcedure {
+	for _, step := range r.OrderedSteps {
+		if step.NextProcedure != nil {
+			return step.NextProcedure
+		}
+	}
+	return nil
 }
 
 // gets key's value from response body, then does operation on it
