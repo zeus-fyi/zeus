40ea39e90edc3bb4d022093aaee890acbea457a2
alex <alexg89@vt.edu>
2023-08-24 16:53:41 -0700

    refactor lib

--- zeus/iris_programmable_proxy/v1beta/iris_v1beta.go
+++ zeus/iris_programmable_proxy/v1beta/iris_v1beta.go
@@ -1,18 +1,15 @@
 package iris_programmable_proxy_v1_beta
 
-import (
-	"github.com/zeus-fyi/zeus/zeus/iris_programmable_proxy"
-)
-
-type IrisV1Beta struct {
-	iris_programmable_proxy.Iris
-}
-
-func NewIrisV1BetaClient(irisBase iris_programmable_proxy.Iris) IrisV1Beta {
-	return IrisV1Beta{
-		irisBase,
-	}
-}
+//
+//type IrisV1Beta struct {
+//	iris_programmable_proxy.Iris
+//}
+//
+//func NewIrisV1BetaClient(irisBase iris_programmable_proxy.Iris) IrisV1Beta {
+//	return IrisV1Beta{
+//		irisBase,
+//	}
+//}
 
 //func (i *IrisV1Beta) CreateProcedure(ctx context.Context, procedure IrisRoutingProcedure) error {
 //	hc := hestia_client.NewHestia(i.GetHestiaRoute(), i.Token)
