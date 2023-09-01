d67930222eede92d314e8d8d5acced3ecdbf509d
alex <alexg89@vt.edu>
2023-09-01 14:45:51 -0700

    adds adaptive headers

--- zeus/iris_programmable_proxy/rpc_loadbalancing_test.go
+++ zeus/iris_programmable_proxy/rpc_loadbalancing_test.go
@@ -49,7 +49,7 @@ func (t *IrisConfigTestSuite) TestParallelRPCLoadBalancing() {
 	web3a := web3_actions.NewWeb3ActionsClient(path)
 	web3a.AddRoutingGroupHeader(routeGroup)
 	web3a.Headers[iris_programmable_proxy_v1_beta.LoadBalancingStrategy] = iris_programmable_proxy_v1_beta.Adaptive
-	web3a.Headers[iris_programmable_proxy_v1_beta.AdaptiveLoadBalancingKey] = iris_programmable_proxy_v1_beta.AdaptiveLoadBalancingKey
+	web3a.Headers[iris_programmable_proxy_v1_beta.AdaptiveLoadBalancingKey] = iris_programmable_proxy_v1_beta.JsonRpcAdaptiveMetrics
 	web3a.AddBearerToken(t.BearerToken)
 	web3a.Dial()
 	defer web3a.Close()
