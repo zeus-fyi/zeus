8863bc1dbe879cb3ba88cac3c66df11f3eed937b
alex <alexg89@vt.edu>
2023-09-27 16:26:28 -0700

    adds digital ocean template wip nvme local pv

--- zeus/cluster_resources/nvme/do/do_nvme.go
+++ zeus/cluster_resources/nvme/do/do_nvme.go
@@ -1,1 +1,11 @@
 package do_nvme
+
+const (
+	DoStorageClass = "fast-disks"
+	DoNvmePath     = "/mnt/fast-disks"
+)
+
+func AddDoNvmeLabels(labels map[string]string) map[string]string {
+	labels["fast-disk-node"] = "pv-raid"
+	return labels
+}
