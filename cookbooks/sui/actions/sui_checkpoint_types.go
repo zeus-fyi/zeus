e33a48dd006fc656c67eac367e0ef5b028053fe1
alex <alexg89@vt.edu>
2023-09-28 12:01:42 -0700

    adds chain id to client

--- cookbooks/sui/actions/sui_checkpoint_types.go
+++ cookbooks/sui/actions/sui_checkpoint_types.go
@@ -24,3 +24,32 @@ type SuiCheckpointResponse struct {
 		Message string `json:"message"`
 	} `json:"error,omitempty"`
 }
+
+type SuiCheckpointsPageResponse struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Result  struct {
+		Data []struct {
+			Epoch                      string `json:"epoch"`
+			SequenceNumber             string `json:"sequenceNumber"`
+			Digest                     string `json:"digest"`
+			NetworkTotalTransactions   string `json:"networkTotalTransactions"`
+			PreviousDigest             string `json:"previousDigest"`
+			EpochRollingGasCostSummary struct {
+				ComputationCost         string `json:"computationCost"`
+				StorageCost             string `json:"storageCost"`
+				StorageRebate           string `json:"storageRebate"`
+				NonRefundableStorageFee string `json:"nonRefundableStorageFee"`
+			} `json:"epochRollingGasCostSummary"`
+			TimestampMs           string        `json:"timestampMs"`
+			Transactions          []string      `json:"transactions"`
+			CheckpointCommitments []interface{} `json:"checkpointCommitments"`
+			ValidatorSignature    string        `json:"validatorSignature"`
+		} `json:"data"`
+		NextCursor  string `json:"nextCursor"`
+		HasNextPage bool   `json:"hasNextPage"`
+	} `json:"result"`
+	Error struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	} `json:"error,omitempty"`
+}
--- cookbooks/sui/actions/sui_checkpoints.go
+++ cookbooks/sui/actions/sui_checkpoints.go
@@ -8,8 +8,6 @@ import (
 	"github.com/zeus-fyi/zeus/zeus/z_client/zeus_common_types"
 )
 
-// https://docs.sui.io/sui-jsonrpc#sui_getCheckpoints
-
 var (
 	GetLatestCheckpointSequenceNumberReq = JsonRpcReq{
 		Jsonrpc: "2.0",
@@ -17,16 +15,49 @@ var (
 		Method:  "sui_getLatestCheckpointSequenceNumber",
 		Params:  []interface{}{},
 	}
-	CheckpointReq = JsonRpcReq{
+	GetCheckpointReq = JsonRpcReq{
 		Jsonrpc: "2.0",
 		Id:      1,
 		Method:  "sui_getCheckpoint",
 		Params:  []interface{}{},
 	}
+	// GetCheckpointsReq https://docs.sui.io/sui-jsonrpc#sui_getCheckpoints
+	GetCheckpointsReq = JsonRpcReq{
+		Jsonrpc: "2.0",
+		Id:      1,
+		Method:  "sui_getCheckpoints",
+		Params:  []interface{}{},
+	}
 )
 
+type SuiCheckpointsRange struct {
+	Cursor          string `json:"cursor,omitempty"`
+	Limit           int    `json:"limit,omitempty"`
+	DescendingOrder bool   `json:"descendingOrder,omitempty"`
+}
+
+func (s *SuiActionsClient) GetCheckpoints(ctx context.Context, cloudCtxNs zeus_common_types.CloudCtxNs, ckpRange SuiCheckpointsRange) ([]SuiCheckpointsPageResponse, error) {
+	req := GetCheckpointsReq
+	req.Params = []interface{}{ckpRange.Cursor, ckpRange.Limit, ckpRange.DescendingOrder}
+	resp, err := s.SendRpcPayload(ctx, cloudCtxNs, req)
+	if err != nil {
+		return []SuiCheckpointsPageResponse{}, err
+	}
+	ss := make([]SuiCheckpointsPageResponse, len(resp.ReplyBodies))
+	i := 0
+	for _, v := range resp.ReplyBodies {
+		err = json.Unmarshal(v, &ss[i])
+		if err != nil {
+			log.Err(err).Msg("SuiActionsClient: GetCheckpoints")
+			return ss, err
+		}
+		i += 1
+	}
+	return ss, err
+}
+
 func (s *SuiActionsClient) GetCheckpoint(ctx context.Context, cloudCtxNs zeus_common_types.CloudCtxNs, ckpId string) ([]SuiCheckpointResponse, error) {
-	req := CheckpointReq
+	req := GetCheckpointReq
 	req.Params = append(req.Params, ckpId)
 	resp, err := s.SendRpcPayload(ctx, cloudCtxNs, req)
 	if err != nil {
