c3972a5700cb9e1c161f9ee39a1859e3872bf60e
alex <alexg89@vt.edu>
2023-10-04 15:48:50 -0700

    refactors test

--- cookbooks/sui/sui_test.go
+++ cookbooks/sui/sui_test.go
@@ -14,7 +14,6 @@ import (
 )
 
 func (t *SuiCookbookTestSuite) TestDeploy() {
-	t.TestUpload()
 	cdep := suiNodeDefinition.GenerateDeploymentRequest()
 
 	_, err := t.ZeusTestClient.DeployCluster(ctx, cdep)
@@ -28,55 +27,6 @@ func (t *SuiCookbookTestSuite) TestDestroy() {
 	resp, err := t.ZeusTestClient.DestroyDeploy(ctx, d)
 	t.Require().Nil(err)
 	t.Assert().NotEmpty(resp)
-}
-
-func (t *SuiCookbookTestSuite) TestUpload() {
-	cfg := SuiConfigOpts{
-		WithLocalNvme:    true,
-		DownloadSnapshot: false,
-		WithIngress:      false,
-		CloudProvider:    "aws",
-		Network:          mainnet,
-	}
-	cd := GetSuiClientClusterDef(cfg)
-	cd.ClusterClassName = "sui"
-	_, err := cd.UploadChartsFromClusterDefinition(ctx, t.ZeusTestClient, true)
-	t.Require().Nil(err)
-}
-
-func (t *SuiCookbookTestSuite) TestUploadTestnet() {
-	cfg := SuiConfigOpts{
-		WithLocalNvme:        true,
-		DownloadSnapshot:     true,
-		WithIngress:          true,
-		WithServiceMonitor:   true,
-		WithArchivalFallback: true,
-		CloudProvider:        "do",
-		Network:              testnet,
-	}
-	suiNodeDefinition = GetSuiClientClusterDef(cfg)
-	t.Require().NotNil(suiNodeDefinition.ComponentBases[Sui])
-	//t.TestCreateClusterClass()
-	t.Require().Equal("sui-testnet-do", suiNodeDefinition.ClusterClassName)
-	_, rerr := suiNodeDefinition.UploadChartsFromClusterDefinition(ctx, t.ZeusTestClient, true)
-	t.Require().Nil(rerr)
-}
-
-func (t *SuiCookbookTestSuite) TestUploadDevnet() {
-	cfg := SuiConfigOpts{
-		WithLocalNvme:      true,
-		DownloadSnapshot:   true,
-		WithIngress:        false,
-		WithServiceMonitor: false,
-		CloudProvider:      "do",
-		Network:            devnet,
-	}
-	suiNodeDefinition = GetSuiClientClusterDef(cfg)
-	t.Require().NotNil(suiNodeDefinition.ComponentBases[Sui])
-	//t.TestCreateClusterClass()
-	t.Require().Equal("sui-devnet-do", suiNodeDefinition.ClusterClassName)
-	_, rerr := suiNodeDefinition.UploadChartsFromClusterDefinition(ctx, t.ZeusTestClient, true)
-	t.Require().Nil(rerr)
 }
 
 func (t *SuiCookbookTestSuite) TestCreateClusterClass() {
