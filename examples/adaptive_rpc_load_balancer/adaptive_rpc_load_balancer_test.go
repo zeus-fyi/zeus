afd331e2e3b9e3772a7368b66b5258269fe93bb3
alex <alexg89@vt.edu>
2023-11-02 21:59:00 -0700

    parallel testing

--- examples/adaptive_rpc_load_balancer/adaptive_rpc_load_balancer_test.go
+++ examples/adaptive_rpc_load_balancer/adaptive_rpc_load_balancer_test.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/stretchr/testify/suite"
+	"github.com/zeus-fyi/gochain/web3/accounts"
+	"github.com/zeus-fyi/zeus/examples/adaptive_rpc_load_balancer/smart_contract_library"
 	web3_actions "github.com/zeus-fyi/zeus/pkg/artemis/web3/client"
 	"github.com/zeus-fyi/zeus/test/configs"
 	"github.com/zeus-fyi/zeus/test/test_suites"
@@ -18,13 +21,30 @@ var ctx = context.Background()
 
 type AdaptiveRpcLoadBalancerExamplesTestSuite struct {
 	test_suites.BaseTestSuite
-	Tc          configs.TestContainer
-	Web3Actions web3_actions.Web3Actions
+	Tc configs.TestContainer
 }
 
 func (s *AdaptiveRpcLoadBalancerExamplesTestSuite) SetupTest() {
 	// points dir to test/configs
 	s.Tc = configs.InitLocalTestConfigs()
+}
+
+func CreateLocalUser(ctx context.Context, bearer, sessionID string) web3_actions.Web3Actions {
+	acc, err := accounts.CreateAccount()
+	if err != nil {
+		panic(err)
+	}
+	w3a := web3_actions.NewWeb3ActionsClientWithAccount(LoadBalancerAddress, acc)
+	w3a.AddAnvilSessionLockHeader(sessionID)
+	w3a.AddBearerToken(bearer)
+	nvB := (*hexutil.Big)(smart_contract_library.EtherMultiple(10000))
+	w3a.Dial()
+	defer w3a.Close()
+	err = w3a.SetBalance(ctx, w3a.Address().String(), *nvB)
+	if err != nil {
+		panic(err)
+	}
+	return w3a
 }
 
 func TestAdaptiveRpcLoadBalancerExamplesTestSuite(t *testing.T) {
--- examples/adaptive_rpc_load_balancer/serverless_evms_test.go
+++ examples/adaptive_rpc_load_balancer/serverless_evms_test.go
@@ -1,36 +1,16 @@
 package adaptive_rpc_load_balancer_examples
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/google/uuid"
 	"github.com/zeus-fyi/gochain/web3/accounts"
 	"github.com/zeus-fyi/zeus/examples/adaptive_rpc_load_balancer/smart_contract_library"
 	web3_actions "github.com/zeus-fyi/zeus/pkg/artemis/web3/client"
 	signing_automation_ethereum "github.com/zeus-fyi/zeus/pkg/artemis/web3/signing_automation/ethereum"
 )
-
-func CreateLocalUser(ctx context.Context, bearer, sessionID string) web3_actions.Web3Actions {
-	acc, err := accounts.CreateAccount()
-	if err != nil {
-		panic(err)
-	}
-	w3a := web3_actions.NewWeb3ActionsClientWithAccount(LoadBalancerAddress, acc)
-	w3a.AddAnvilSessionLockHeader(sessionID)
-	w3a.AddBearerToken(bearer)
-	nvB := (*hexutil.Big)(smart_contract_library.EtherMultiple(10000))
-	w3a.Dial()
-	defer w3a.Close()
-	err = w3a.SetBalance(ctx, w3a.Address().String(), *nvB)
-	if err != nil {
-		panic(err)
-	}
-	return w3a
-}
 
 func (s *AdaptiveRpcLoadBalancerExamplesTestSuite) setupMintToken(mintAmount *big.Int) (string, web3_actions.SendContractTxPayload) {
 	// mintable contract
