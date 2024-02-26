d460e8fb5f2169df13480411c84f2f683a154fe3
alex <alexg89@vt.edu>
2024-02-25 17:38:48 -0800

    add schema ex

--- examples/mockingbird/python/evals.py
+++ examples/mockingbird/python/evals.py
@@ -1,6 +1,26 @@
+import json  # Import the json module
+
+import requests
+
+from examples.mockingbird.python.api_setup import get_headers, api_v1_path
+
+
 def get_evals():
-    pass
+    url = api_v1_path + "/evals/ai"
+    headers = get_headers()
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        pretty_data = json.dumps(data, indent=4)
+        print(pretty_data)
+    else:
+        print("Status Code:", response.status_code)
 
 
 def create_or_update_eval():
     pass
+
+
+if __name__ == '__main__':
+    with open('templates/eval_fn.json', 'r') as file:
+        payload = json.load(file)
--- examples/mockingbird/python/triggers.py
+++ examples/mockingbird/python/triggers.py
@@ -1,6 +1,26 @@
+import json  # Import the json module
+
+import requests
+
+from examples.mockingbird.python.api_setup import get_headers, api_v1_path
+
+
 def get_triggers():
-    pass
+    url = api_v1_path + "/actions/ai"
+    headers = get_headers()
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        pretty_data = json.dumps(data, indent=4)
+        print(pretty_data)
+    else:
+        print("Status Code:", response.status_code)
 
 
 def create_or_update_trigger():
     pass
+
+
+if __name__ == '__main__':
+    with open('templates/triggers.json', 'r') as file:
+        payload = json.load(file)
