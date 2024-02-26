afcd97bc6003bffbcba87e084596079fec102043
alex <alexg89@vt.edu>
2024-02-25 17:17:44 -0800

    add schema ex

--- examples/mockingbird/python/schemas.py
+++ examples/mockingbird/python/schemas.py
@@ -2,30 +2,52 @@ import json  # Import the json module
 
 import requests
 
-from examples.mockingbird.python.api_setup import get_headers
-
-url = "https://api.zeus.fyi/v1/schemas/ai"
+from examples.mockingbird.python.api_setup import get_headers, api_v1_path
 
 
 def get_schemas():
+    url = api_v1_path + "/schemas/ai"
     headers = get_headers()
     response = requests.get(url, headers=headers)
-
     if response.status_code == 200:
-        data = response.json()
         pretty_data = json.dumps(data, indent=4)
         print(pretty_data)
     else:
         print("Status Code:", response.status_code)
 
 
-def create_or_update_schema():
+def create_or_update_schema(schema):
+    url = api_v1_path + "/schemas/ai"
     headers = get_headers()
-    response = requests.get(url, headers=headers)
-
+    response = requests.post(url, json=schema, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        pretty_data = json.dumps(data, indent=4)
-        print(pretty_data)
+        print("Schema created successfully!")
     else:
-        print("Status Code:", response.status_code)
+        print(response.json())
+        print("Failed to create schema. Status Code:", response.status_code)
+
+
+field_data_template = {
+    "fieldID": 0,
+    "fieldStrID": "0",
+    "fieldName": "msg_id",
+    "fieldDescription": "The analyzed id value.",
+    "dataType": "string"
+}
+
+if __name__ == '__main__':
+    with open('templates/schema.json', 'r') as file:
+        data = json.load(file)
+
+    data['schemaName'] = 'demo-schema'
+    data['schemaGroup'] = 'demo'
+    data['schemaDescription'] = 'A schema for the demo task.'
+    data['isObjArray'] = True
+
+    field_data_template['fieldName'] = 'msg_id'
+    field_data_template['fieldDescription'] = 'The analyzed id value.'
+    field_data_template['dataType'] = 'string'
+    field_data_template['description'] = 'string'
+    data['fields'] = [field_data_template]
+
+    create_or_update_schema(data)
